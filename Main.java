61c2a5e56375dac23ace206d9afea29285fc5263
kailasa-Nischal <78311217+kailasa-Nischal@users.noreply.github.com>
2022-07-08 14:12:37 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,124 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            FastReader in = new FastReader();
+            FastWriter out = new FastWriter();
+            int testCases = in.nextInt();
+            while (testCases-- > 0) {
+                //int n = in.nextInt();
+                String[] s = new String[2];
+                s = in.nextLine().split(" ");
+                int n = Integer.parseInt(s[0]), m = Integer.parseInt(s[1]);
+                //  String s=in.next();
+                 int[] a=new int[n];
+                String[] s1=new String[n];
+                s1=in.nextLine().split(" ");   
+                int sum=0;                           
+                for (int i = 0; i <n; i++){                               
+                    a[i]=Integer.parseInt(s1[i]); 
+                    sum+=a[i];                        
+                }                
+                //long m=1000000007;                
+                if(m>sum)   out.println(0);
+                else    out.println(sum-m);
+
+               
+               
+                
+                                
+        }
+            
+
+            out.close();
+        } catch (
+
+        Exception e) {
+            System.out.print(e);
+            return;
+        }
+    }
+
+
+    public static class Pair implements Comparable<Pair> {
+        int val, t;
+
+        public Pair(int val, int t) {
+            this.t = t;
+            this.val = val;
+        }
+
+        public int compareTo(Pair o) {
+            if (o.val == this.val)
+                return this.t - o.t; // ascending order
+            else
+                return o.val - this.val; // desending order
+        }
+    }
+
+}
